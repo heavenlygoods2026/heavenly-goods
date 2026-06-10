58f6bcc079deee8e51644d9afad3dcd7340d3308
Juan Tejeda <juanga.tejeda@gmail.com>
2026-06-10 00:25:36 -0400

    chore: move firebase config to environment variables to fix secret scan alert

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,13 +5,13 @@ import { getStorage } from 'firebase/storage';
 import { getFunctions } from 'firebase/functions';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyASXI_2WzQ-X3afWpkvpPW-C6uHXUyLcdM",
-  authDomain: "heavenly-goods.firebaseapp.com",
-  projectId: "heavenly-goods",
-  storageBucket: "heavenly-goods.firebasestorage.app",
-  messagingSenderId: "718413199981",
-  appId: "1:718413199981:web:4efd8d9b04aa7e03ca5d64",
-  measurementId: "G-9TL2G2835F"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
